4bd0daf6238c5208f06d725940d163fba2d1d149
jomendozac <141792489+jomendozac@users.noreply.github.com>
2024-11-17 13:25:52 -0500

    Create Ej14 Entrega 1.java

---
+++ Ej14 Entrega 1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej14.entrega.pkg1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ej14Entrega1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         double number, squared, cubic;
+        
+        number = 7;
+        squared = Math.pow(number, 2);
+        cubic = Math.pow(number, 3);
+        
+        System.out.println("El numero es: " + number);
+        System.out.println("El cuadrado del numero es: " + squared);
+        System.out.println("El cubo del numero es: " + cubic);
+    }
+    
+}
