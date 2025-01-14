d602a28d02a2eff00ed9b02f2eed89db9ef2ba52
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-14 17:24:26 -0500

    Create EJRES7.java

---
+++ EJRES7.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.ejres7;
+    import Interfaz.Pantalla_Ej7;
+public class EJRES7 {
+
+    public static void main(String[] args) {
+        
+        Pantalla_Ej7 panta = new Pantalla_Ej7 ();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+    }
+}
