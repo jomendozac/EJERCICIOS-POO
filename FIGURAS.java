4286da9142a76571eae8c4edff3956a520431323
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-14 18:05:36 -0500

    Create FIGURAS.java

---
+++ FIGURAS.java
@@ -0,0 +1,11 @@
+package com.mycompany.figuras;
+
+public class FIGURAS {
+
+    public static void main(String[] args) {
+        
+        Pantalla Principal = new Pantalla();
+        Principal.setVisible(true);
+        Principal.setLocationRelativeTo(null);
+    }
+}
