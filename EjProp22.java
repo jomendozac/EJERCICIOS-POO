174c223ca34ada5fd73b46b97f360466aa4aebc3
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-14 17:41:35 -0500

    Create EjProp22.java

---
+++ EjProp22.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejprop22;
+
+import Interfaz.Ejercicio_22;
+
+public class EjProp22 {
+
+    public static void main(String[] args) {
+        Ejercicio_22 panta = new Ejercicio_22 ();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
