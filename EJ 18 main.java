ff85f027b33db939556975446ba7f114da1c8742
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-14 17:00:57 -0500

    Create EJ 18 main.java

---
+++ EJ 18 main.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejprop18;
+
+import Interfaz.Ejercicio_18;
+
+public class EjProp18 {
+
+    public static void main(String[] args) {
+        Ejercicio_18 panta = new Ejercicio_18 ();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
