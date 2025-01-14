0f40c8dd622ea1534936f5662a0b01ed7b7c6354
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-14 17:09:57 -0500

    Create Ej 19 main.java

---
+++ Ej 19 main.java
@@ -0,0 +1,12 @@
+package com.mycompany.ejprop19;
+
+import Interfaz.Ejercicio_19;
+
+public class EjProp19 {
+
+    public static void main(String[] args) {
+        Ejercicio_19 panta = new Ejercicio_19 ();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
