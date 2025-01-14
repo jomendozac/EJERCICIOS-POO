d143a68317863a5d3fe89d14ebc98feaa2393282
jomendozac <141792489+jomendozac@users.noreply.github.com>
2025-01-14 17:33:01 -0500

    Create EJRES10.java

---
+++ EJRES10.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.ejres10;
+
+import Interfaz.Pantalla_Ej10;
+
+public class EJRES10 {
+
+    public static void main(String[] args) {
+        
+        Pantalla_Ej10 panta = new Pantalla_Ej10 ();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+
+    }
+    
+}
